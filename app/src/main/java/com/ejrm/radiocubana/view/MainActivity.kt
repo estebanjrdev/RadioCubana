3fe109c5000ff38a84cd850ac3dbbe1c748466bb
EstebanJR.Dev <susoluciones.software@gmail.com>
2022-12-01 16:35:22 +0100

    List de emisoras

--- app/src/main/java/com/ejrm/radiocubana/view/MainActivity.kt
+++ app/src/main/java/com/ejrm/radiocubana/view/MainActivity.kt
@@ -7,13 +7,10 @@ import com.ejrm.radiocubana.R
 import com.ejrm.radiocubana.databinding.ActivityMainBinding
 import com.ejrm.radiocubana.model.EmisoraModel
 import com.ejrm.radiocubana.model.EmisoraProvider
+import com.ejrm.radiocubana.model.EmisoraProvider.Companion.emisoraList
 import com.ejrm.radiocubana.view.adapters.EmisoraAdapter
 
 class MainActivity : AppCompatActivity() {
-    var emisoraList: List<EmisoraModel> = listOf(
-    EmisoraModel("https://icecast.teveo.cu/PKWhw37L", "Radio Bahía", "", R.drawable.radio_bahia),
-    EmisoraModel("https://icecast.teveo.cu/srJ4vqkv", "Radio Caibarién", "", R.drawable.radio_caibarien),
-    EmisoraModel("https://icecast.teveo.cu/fvc4RVRz", "Radio Coco", "", R.drawable.radio_coco))
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
