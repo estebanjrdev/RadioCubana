2ea7a172dc8320022020513ec3d1a411301a29a7
estebanjr.dev <tuapkmovil@gmail.com>
2022-12-10 10:13:25 -0600

    ///

--- app/src/main/java/com/ejrm/radiocubana/view/MainActivity.kt
+++ app/src/main/java/com/ejrm/radiocubana/view/MainActivity.kt
@@ -5,11 +5,16 @@ import android.media.AudioManager
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.ejrm.radiocubana.R
 import com.ejrm.radiocubana.databinding.ActivityMainBinding
 import com.ejrm.radiocubana.model.EmisoraModel
 import com.ejrm.radiocubana.model.EmisoraProvider
 import com.ejrm.radiocubana.view.adapters.EmisoraAdapter
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -30,6 +35,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun itemSelected(emisora: EmisoraModel) {
+        binding.layoutReproduction.visibility = LinearLayout.VISIBLE
+        binding.imagelogo.setImageResource(emisora.imagen)
+        binding.tilte.text = emisora.name
+        binding.imagePlay.setImageResource(R.drawable.ic_pause_24)
         val url = emisora.link
         val mediaPlayer: MediaPlayer? = MediaPlayer().apply {
             setAudioStreamType(AudioManager.STREAM_MUSIC)
