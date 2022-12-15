31f292d35233e5a1c904836c9c4e776e82669aeb
estebanjr.dev <tuapkmovil@gmail.com>
2022-12-15 12:10:48 -0600

    modify adapter

--- app/src/main/java/com/ejrm/radiocubana/view/adapters/EmisoraAdapter.kt
+++ app/src/main/java/com/ejrm/radiocubana/view/adapters/EmisoraAdapter.kt
@@ -1,8 +1,5 @@
 package com.ejrm.radiocubana.view.adapters
 
-import android.content.Context
-import android.content.Intent
-import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,7 +7,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.ejrm.radiocubana.R
 import com.ejrm.radiocubana.databinding.EmisoraCardBinding
 import com.ejrm.radiocubana.model.EmisoraModel
-import com.ejrm.radiocubana.view.EmisoraView
 
 
 class EmisoraAdapter(
